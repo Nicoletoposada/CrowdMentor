b4b9d51a477c4df03edecbf9945502eb79290dcc
Nicoletoposada <nicoletoposada@gmail.com>
2026-02-25 08:43:31 -0500

    Crowdmentor

--- crowdmentor/core/admin.py
+++ crowdmentor/core/admin.py
@@ -1,13 +1,53 @@
 from django.contrib import admin # type: ignore
-from .models import Profile, Project, Investment, Mentorship, Message, ResourceCategory, Resource
+from django.utils.html import format_html # type: ignore
+from .models import Profile, Project, Investment, Mentorship, Message, ResourceCategory, Resource, UploadedFile
 
 # Register your models here.
 
+class UploadedFileInline(admin.TabularInline):
+    model = UploadedFile
+    extra = 0
+    readonly_fields = ['file_preview', 'file', 'description', 'uploaded_at']
+    fields = ['file_preview', 'file', 'description', 'uploaded_at']
+    can_delete = False
+
+    def file_preview(self, obj):
+        if obj.file:
+            url = obj.file.url
+            name = obj.file.name.split('/')[-1]
+            ext = name.split('.')[-1].lower()
+            if ext in ['jpg', 'jpeg', 'png', 'gif', 'webp', 'jfif']:
+                return format_html('<a href="{}" target="_blank"><img src="{}" style="max-height:80px;max-width:120px;"/></a>', url, url)
+            return format_html('<a href="{}" target="_blank">{}</a>', url, name)
+        return '-'
+    file_preview.short_description = 'Vista previa'
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'user_type', 'is_approved', 'is_approved_by_admin']
+    list_display = ['user', 'user_type', 'is_approved', 'is_approved_by_admin', 'num_archivos']
     list_filter = ['user_type', 'is_approved', 'is_approved_by_admin']
     search_fields = ['user__username', 'user__email']
+    inlines = [UploadedFileInline]
+
+    def num_archivos(self, obj):
+        count = obj.uploaded_files.count()
+        return count
+    num_archivos.short_description = 'Archivos subidos'
+
+@admin.register(UploadedFile)
+class UploadedFileAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'file_link', 'description', 'uploaded_at']
+    list_filter = ['uploaded_at', 'profile__user_type']
+    search_fields = ['profile__user__username', 'description']
+    readonly_fields = ['uploaded_at']
+
+    def file_link(self, obj):
+        if obj.file:
+            url = obj.file.url
+            name = obj.file.name.split('/')[-1]
+            return format_html('<a href="{}" target="_blank">{}</a>', url, name)
+        return '-'
+    file_link.short_description = 'Archivo'
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
